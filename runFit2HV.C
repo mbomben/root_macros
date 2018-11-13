49e8099f738b6b9039501a86eb8e6868097fefcb
Marco Bomben <marco.bomben@cern.ch>
2018-11-13 23:20:54 +0100

    runFit2HV.C: macro to run Fit2HV.C

--- runFit2HV.C
+++ runFit2HV.C
@@ -9,7 +9,7 @@ void runFit2HV(const char* fileName, float &rms, float prec = 0.02)
   Fit2HV(gre,prec,rms);
 
   gre->Draw("A*E");
-  f1->Draw("LSAME");
-  f2->Draw("LSAME");
+  //f1->Draw("LSAME");
+  //f2->Draw("LSAME");
 }
   
